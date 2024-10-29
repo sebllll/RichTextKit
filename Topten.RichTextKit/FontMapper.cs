a68303594a88a10191339b20a5853e8e721a8225
Alexander Shaliapin <alexander@m-box.de>
2024-10-29 13:16:00 +0100

    adds functionality to load a font from local file and use it in the style

--- Topten.RichTextKit/FontMapper.cs
+++ Topten.RichTextKit/FontMapper.cs
@@ -17,6 +17,7 @@ using SkiaSharp;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace RichTextKit
 {
@@ -26,11 +27,44 @@ namespace RichTextKit
     /// </summary>
     public class FontMapper
     {
+        static Dictionary<string, List<SKTypeface>> _customFonts = new Dictionary<string, List<SKTypeface>>();
+        
         /// <summary>
         /// Constructs a new FontMapper instnace
         /// </summary>
         public FontMapper()
         {
+        }
+
+        /// <summary>
+        /// Loads a private font from a stream
+        /// </summary>
+        /// <param name="stream">The stream to load from</param>
+        /// <param name="familyName">An optional family name to override the font's built in name</param>
+        /// <returns>True if the font was successully loaded</returns>
+        public static bool LoadPrivateFont(System.IO.Stream stream, string familyName = null)
+        {
+            var tf = SKTypeface.FromStream(stream);
+            if (tf == null)
+                return false;
+
+            var qualifiedName = familyName ?? tf.FamilyName;
+            if (tf.FontSlant != SKFontStyleSlant.Upright)
+            {
+                qualifiedName += "-Italic";
+            }
+
+            // Get a list of typefaces with this family
+            if (!_customFonts.TryGetValue(qualifiedName, out var listFonts))
+            {
+                listFonts = new List<SKTypeface>();
+                _customFonts[qualifiedName] = listFonts;
+            }
+
+            // Add to the list
+            listFonts.Add(tf);
+
+            return true;
         }
 
         /// <summary>
@@ -41,20 +75,41 @@ namespace RichTextKit
         /// <returns>A mapped typeface</returns>
         public virtual SKTypeface TypefaceFromStyle(IStyle style, bool ignoreFontVariants)
         {
-            // Extra weight for superscript/subscript
-            int extraWeight = 0;
-            if (!ignoreFontVariants && (style.FontVariant == FontVariant.SuperScript || style.FontVariant == FontVariant.SubScript))
+            // Work out the qualified name
+            var qualifiedName = style.FontFamily;
+            if (style.FontItalic)
+                qualifiedName += "-Italic";
+
+            // Look up custom fonts
+            List<SKTypeface> listFonts;
+            if (_customFonts.TryGetValue(qualifiedName, out listFonts))
             {
-                extraWeight += 100;
+                // Find closest weight
+                listFonts.OrderBy(x => Math.Abs(x.FontWeight - style.FontWeight));
+                return listFonts[0];
+            }
+            
+            else
+            {
+                
+                // Extra weight for superscript/subscript
+                int extraWeight = 0;
+                if (!ignoreFontVariants && (style.FontVariant == FontVariant.SuperScript || style.FontVariant == FontVariant.SubScript))
+                {
+                    extraWeight += 100;
+                }
+
+                // Get the typeface
+                return SKTypeface.FromFamilyName(
+                    style.FontFamily, 
+                    (SKFontStyleWeight)(style.FontWeight + extraWeight), 
+                    style.FontWidth, 
+                    style.FontItalic ? SKFontStyleSlant.Italic : SKFontStyleSlant.Upright
+                    ) ?? SKTypeface.CreateDefault();
             }
 
-            // Get the typeface
-            return SKTypeface.FromFamilyName(
-                style.FontFamily, 
-                (SKFontStyleWeight)(style.FontWeight + extraWeight), 
-                style.FontWidth, 
-                style.FontItalic ? SKFontStyleSlant.Italic : SKFontStyleSlant.Upright
-                ) ?? SKTypeface.CreateDefault();
+            // Do default mapping
+            //return SKTypeface.Default
         }
 
         /// <summary>
--- Topten.RichTextKit/Style.cs
+++ Topten.RichTextKit/Style.cs
@@ -16,7 +16,7 @@
 using SkiaSharp;
 using System;
 using System.Collections.Generic;
-using System.Text;
+using System.IO;
 
 namespace RichTextKit
 {
@@ -30,6 +30,12 @@ namespace RichTextKit
         {
             if (_sealed)
                 throw new InvalidOperationException("Style has been sealed and can't be modified");
+        }
+
+        public void LoadFont(string Path, string Name)
+        {
+            Stream font = File.OpenRead(Path);
+            FontMapper.LoadPrivateFont(font, Name);
         }
 
         /// <summary>
